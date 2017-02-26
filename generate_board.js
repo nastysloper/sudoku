fd13f62ba911a7a4f7bfd69360bcc131fa5b534c
Rich Vogt <richvogtjr@gmail.com>
2017-02-25 21:12:26 -0600

    add a function to print the board

--- generate_board.js
+++ generate_board.js
@@ -1,5 +1,16 @@
+/**
+* A board will be an array of 81 elements.
+*
+*
+*/
 var board = [];
 
-for (i = 0; i < 9; i++) {
+console.log("running");
 
+for (i = 0; i < 81; i++) {
+  board[i] = Math.floor(Math.random(0,9) * 10);
+}
+
+for (i = 0; i < 81; i+=9) {
+  console.log(board[i] + " " + board[i+1] + " " + board[i+2] + " " + board[i+3] + " " + board[i+4] + " " + board[i+5] + " " + board[i+6] + " " + board[i+7] + " " + board[i+8] );
 }
